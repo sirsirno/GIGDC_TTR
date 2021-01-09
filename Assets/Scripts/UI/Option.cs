034d259d6026ea534c459f3253dc65d3c3f404d3
DESKTOP-GQ8UI5N\ggmrj <ggmrjsdnr@gmail.com>
2021-01-09 12:22:29 +0900

    UI 인게임 1번 기본 틀 옵션 1,3번 구현 했음

---
+++ Assets/Scripts/UI/Option.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Option : MonoBehaviour
+{
+    public GameObject menuSet;
+
+    private bool opActiveSelf = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)&&opActiveSelf ==false )
+        {
+            Time.timeScale = 0;
+            menuSet.SetActive(true);
+            opActiveSelf = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape)&& opActiveSelf ==true) 
+        {
+            Time.timeScale = 1;
+            menuSet.SetActive(false);
+            opActiveSelf = false;
+        }
+
+
+    }
+
+    public void OnClickRetBtn() 
+    {
+        Time.timeScale = 1;
+        menuSet.SetActive(false);
+        opActiveSelf = false;
+
+    }
+    public void OnClickResBtn() 
+    {
+        Time.timeScale = 1;
+        menuSet.SetActive(false);
+        opActiveSelf = false;
+        SceneManager.LoadScene(0);
+    }
+}
