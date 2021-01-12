c5520fa2011f8ae8a81e9a5b9562f6db79ef000d
DESKTOP-GQ8UI5N\ggmrj <ggmrjsdnr@gmail.com>
2021-01-12 18:21:25 +0900

    Title 화면 만듬

---
+++ Assets/Scripts/UI/BlinkTxt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BlinkTxt : MonoBehaviour
+{
+    [SerializeField]
+    private Text startTxt = null;
+
+    // Update is called once per frame
+    void Start()
+    {
+        startTxt.DOColor(new Color(1f, 1f, 1f, 10f), 0.8f).SetLoops(-1, LoopType.Yoyo);
+        //GetComponent<MeshRenderer>().material.DOFade(0, 1).SetLoops(-1, LoopType.Yoyo);
+        //GetComponent<SpriteRenderer>().material.DOFade(0, 1).SetLoops(-1, LoopType.Yoyo);
+    }
+}
