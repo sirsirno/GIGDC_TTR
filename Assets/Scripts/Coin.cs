93b0fbd5f705c0a02b1e94dc808ac2ce1d771d6d
DESKTOP-GQ8UI5N\ggmrj <ggmrjsdnr@gmail.com>
2021-02-22 16:16:45 +0900

    코인 끝 이제 죽을 때 드랍 하시면 됨

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class TTRCoin
+{
+    public long coins = 0;
+}
--- Assets/Scripts/Management/GameManager.cs
+++ Assets/Scripts/Management/GameManager.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
+    
     private static GameManager instance = null;
     public static GameManager Instance
     {
@@ -25,6 +31,15 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    [SerializeField]
+    private TTRCoin coin;
+    [SerializeField]
+    private Text textCoin;
+
+    private string filePath = "";
+
+    private string jsonString = "";
+
     void Awake()
     {
         if (instance == null)
@@ -37,7 +52,17 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+        
     }
+
+    private void Start()
+    {
+        coin = new TTRCoin();
+        filePath = string.Concat(Application.persistentDataPath, "/", "TTRCoin.txt");
+        LoadCoin();
+    }
+
+    
 
     public Vector3 MapCenter { get; set; }  // 카메라가 멈출 맵의 중심
     public Vector3 MapInfo { get; set; }  // 맵에 대한 정보 x,y
@@ -47,12 +72,46 @@ public class GameManager : MonoBehaviour
 
     [Header("저장될 게임의 정보")]
     public GameInfo playerInfo;
+
+    public void SaveCoin() 
+    {
+        jsonString = JsonUtility.ToJson(coin);
+        FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+
+        byte[] data = Encoding.UTF8.GetBytes(jsonString);
+        fs.Write(data, 0, data.Length);
+        fs.Close();
+    }
+
+    public void LoadCoin() 
+    {
+        FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+        byte[] data = new byte[fs.Length];
+        fs.Read(data, 0, data.Length);
+        fs.Close();
+
+        jsonString = Encoding.UTF8.GetString(data);
+        coin = JsonUtility.FromJson<TTRCoin>(jsonString);
+        UpdateCoin();
+    }
+    public void UpdateCoin() 
+    {
+        if (textCoin == null) return;
+        textCoin.text = string.Format("{0} 개", coin.coins);
+    }
+    private void OnApplicationQuit()
+    {
+        SaveCoin();
+    }
+
+
 }
 
-[System.Serializable]
+[Serializable]
 public class GameInfo
 {
     public float maxHp;
     public float currentHp;
     public float playerDamage;
+    
 }
