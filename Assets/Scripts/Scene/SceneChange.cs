8ed72e59eb29540c89a25b2979a99fe09f4a31bf
DESKTOP-GQ8UI5N\ggmrj <ggmrjsdnr@gmail.com>
2021-03-06 12:20:56 +0900

    씬이동

---
+++ Assets/Scripts/Scene/SceneChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    private void Update()
+    {
+        if (GameManager.Instance.playerInfo.currentHp <= 0) 
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
--- Assets/Scripts/UI/Title/BlinkTxt.cs
+++ Assets/Scripts/UI/Title/BlinkTxt.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class BlinkTxt : MonoBehaviour
 {
@@ -15,5 +16,12 @@ public class BlinkTxt : MonoBehaviour
         startTxt.DOColor(new Color(1f, 1f, 1f, 10f), 0.8f).SetLoops(-1, LoopType.Yoyo);
         //GetComponent<MeshRenderer>().material.DOFade(0, 1).SetLoops(-1, LoopType.Yoyo);
         //GetComponent<SpriteRenderer>().material.DOFade(0, 1).SetLoops(-1, LoopType.Yoyo);
+    }
+    private void Update()
+    {
+        if (Input.anyKeyDown) 
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
